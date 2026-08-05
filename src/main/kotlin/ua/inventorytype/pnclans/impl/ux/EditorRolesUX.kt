b77de03e098950536109b0aa8ded0fbfeb349788
overdyn <shulkerprotectionbackup@gmail.com>
2026-08-05 22:35:43 +0200

    fix(ux): remove stale EnumMap GUI caching in RolePermissionsUX for reliable reloads

--- src/main/kotlin/ua/inventorytype/pnclans/impl/ux/EditorRolesUX.kt
+++ src/main/kotlin/ua/inventorytype/pnclans/impl/ux/EditorRolesUX.kt
@@ -121,24 +121,6 @@ class RolePermissionsUX(
     val editorRolesUX: EditorRolesUX
 ) : BaseGui(clanService) {
 
-    companion object {
-        private val cache = EnumMap<ClanRole, RolePermissionsUX>(ClanRole::class.java)
-
-        operator fun invoke(clanService: ClanService, role: ClanRole, editorRolesUX: EditorRolesUX): RolePermissionsUX {
-            return cache.getOrPut(role) { RolePermissionsUX.create(clanService, role, editorRolesUX) }
-        }
-
-        private fun create(clanService: ClanService, role: ClanRole, editorRolesUX: EditorRolesUX): RolePermissionsUX =
-            RolePermissionsUX(clanService, role, editorRolesUX, Unit)
-    }
-
-    private constructor(
-        clanService: ClanService,
-        targetRole: ClanRole,
-        editorRolesUX: EditorRolesUX,
-        @Suppress("UNUSED_PARAMETER") marker: Unit
-    ) : this(clanService, targetRole, editorRolesUX)
-
     init {
         val cfg = clanService.plugin.configService
         val menuCfg = cfg.menus.editorRolesMenu
